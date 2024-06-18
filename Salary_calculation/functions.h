da78bec9a35f10664c3afe6df7f11e7dec97302a
villsun <lonukkola@gmail.com>
2024-06-18 23:50:16 +0200

    Add structures Parameters and Day. Add class Job

--- Salary_calculation/functions.h
+++ Salary_calculation/functions.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Parameters
+{
+	double hourly_wage;
+	double supplement_in_weekend;
+	double supplement_in_evening;
+};
+
+struct Day
+{
+	string date;
+	double start_work;
+	double end_work;
+	double number_hours_at_work;
+	double earned_in_day;
+};
+
+class Job
+{
+private:
+	Parameters parameters_in_file;
+	vector<Day> array_day;
+public:
+};
